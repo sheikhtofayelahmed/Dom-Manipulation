7e9c40d226c4f429f5b190da11939797bf028463
tofayel101 <tofayel101@proton.me>
2022-12-08 14:55:38 +0600

    #3- copy hex color

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const target = document.getElementById("root");
 const btn = document.getElementById("btn");
-
+const colorCode = document.getElementById("colorCode");
+const copyBtn = document.getElementById("copy-btn");
+let rgbHex;
 const ColorChange = () => {
   const red = Math.floor(Math.random() * 255).toString(16);
   const green = Math.floor(Math.random() * 255).toString(16);
   const blue = Math.floor(Math.random() * 255).toString(16);
 
-  const rgbHex = `#${red}${green}${blue}`;
+  rgbHex = `#${red}${green}${blue}`;
   return rgbHex;
 };
-
-btn.addEventListener("click", function event() {
+function clickEvent() {
   const rgbHex = ColorChange();
-  console.log(rgbHex);
   target.style.backgroundColor = rgbHex;
+  colorCode.innerHTML = rgbHex;
+}
+window.onload = () => {
+  clickEvent();
+};
+btn.addEventListener("click", clickEvent);
+copyBtn.addEventListener("click", function copy() {
+  navigator.clipboard.writeText(rgbHex);
+  alert(`copied ${rgbHex}`);
 });
