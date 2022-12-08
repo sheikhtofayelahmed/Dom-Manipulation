12dab32f1ecb672b7067c1f304b2c1083bf0718d
tofayel101 <tofayel101@proton.me>
2022-12-08 14:41:16 +0600

    #2 - hexa background color generator

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const target = document.getElementById("root");
 const btn = document.getElementById("btn");
 
 const ColorChange = () => {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
+  const red = Math.floor(Math.random() * 255).toString(16);
+  const green = Math.floor(Math.random() * 255).toString(16);
+  const blue = Math.floor(Math.random() * 255).toString(16);
 
-  const rgb = `rgb(${red},${green},${blue})`;
-  return rgb;
+  const rgbHex = `#${red}${green}${blue}`;
+  return rgbHex;
 };
 
 btn.addEventListener("click", function event() {
-  const rgb = ColorChange();
-  target.style.backgroundColor = rgb;
+  const rgbHex = ColorChange();
+  console.log(rgbHex);
+  target.style.backgroundColor = rgbHex;
 });
